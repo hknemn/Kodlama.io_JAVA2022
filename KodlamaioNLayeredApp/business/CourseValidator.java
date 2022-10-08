4996d08e0bb7e861bedaa9b7fa0da559ac463ad4
Hakan Emin Ünal <20010310044@ogrenci.bartin.edu.tr>
2022-10-08 15:03:22 +0300

    kodlama.io website layered architecture simulation

--- KodlamaioNLayeredApp/business/CourseValidator.java
+++ KodlamaioNLayeredApp/business/CourseValidator.java
@@ -6,7 +6,7 @@ import KodlamaioNLayeredApp.entities.Course;
 
 public class CourseValidator {
 	public static boolean isValid(Course course, List<String> courses) {
-		if(courses.isEmpty()) {
+		if(courses.isEmpty() && course.getCoursePrice()>0) {
 			return true;
 		}else {
 			if(course.getCoursePrice()<=0 || courses.contains(course.getCategoryName()) || courses.contains(course.getCourseName())) {
